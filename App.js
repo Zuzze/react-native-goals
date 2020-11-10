7a04611e03f78e14db14d8d7a435e7db5f965594
Zuzze <nevalsus@gmail.com>
2020-11-11 00:09:52 +0100

    goal input and list added

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+  Button
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const handleGoalInputChange = text => {
+    setEnteredGoal(text);
+    console.log(text);
+  };
+
+  const handleAddGoal = () => {
+    // goals cana be in previous state snapshot
+    // to make sure you have latest goals, use arrow function
+    setGoals(currentGoals => [...currentGoals, enteredGoal]);
+    console.log("goals", goals);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Goals 2020</Text>
+
+      <Text>Add Goal</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={handleGoalInputChange}
+          value={enteredGoal}
+          style={styles.input}
+        />
+        <Button title="Add" onPress={handleAddGoal} style={{ width: "20%" }} />
+      </View>
+
+      <Text>Goals</Text>
+      <ScrollView>
+        {goals.map(goal => (
+          <View key={goal} style={styles.goalItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +54,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+    //flex: 1,
+    backgroundColor: "#fff"
+    // alignItems: "center",
+    // justifyContent: "center"
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  },
+  goalItem: {
+    backgroundColor: "aliceblue",
+    borderRadius: 30,
+    padding: 20,
+    marginVertical: 10,
+    width: "100%"
+  }
 });
